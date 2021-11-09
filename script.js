c71944c7701e71900e94fbe74a556a68d97475bc
Yaroslav <petrowskiy141@gmail.com>
2021-11-09 17:41:07 +0200

    tack1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hello');
+let numberOfFilms = +prompt("Сколько фильмов ви уже посмотрели",'');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
+prompt("Один из последних просмотених фильмов?");
+
+prompt("На сколько оцените его?");
+console.log(personalMovieDB.movies);
+
